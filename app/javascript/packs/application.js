57d1686ca2d32039b3455494d0f55ab0e3a93310
betsy_manuel <betsy.manuel03@gmail.com>
2021-01-12 18:27:47 -0800

    fixed destroy method

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,9 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../stylesheets/application'
+// import '../stylesheets/application'
 
 Rails.start()
 ActiveStorage.start()
+
+console.log("hello world")
